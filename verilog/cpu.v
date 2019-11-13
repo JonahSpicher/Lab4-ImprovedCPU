fbe3c6db3e99455cbf4305ca3e7dd23d2550b460
JonahSpicher <jonah.spicher@gmail.com>
2019-11-13 16:27:36 -0500

    Fixed CPU wire stuff

--- verilog/cpu.v
+++ verilog/cpu.v
@@ -75,7 +75,7 @@ module reg_bank(
   output [4:0] RS_ready,
   output [4:0] RT_1,
   output [4:0] RT_ready,
-  output [4:0] RD_ready.
+  output [4:0] RD_ready,
   output [15:0] imm_ready,
   output Wren_ready,
   output [2:0] ALU_ctrl_ready,
@@ -84,7 +84,7 @@ module reg_bank(
   input [31:0] PC,
   input [4:0] RS,
   input [4:0] RT,
-  input [4:0] RD.
+  input [4:0] RD,
   input [15:0] imm,
   input Wren,
   input [2:0] ALU_ctrl,
@@ -93,42 +93,50 @@ module reg_bank(
   input clk,
   input reset
 );
-  register32 PC_IF_RF(.q(PC_1), .d(PC), .wrenable(1'd1), .clk(clk), .reset(reset));
-  register32 PC_RF_EX(.q(PC_2), .d(PC_1), .wrenable(1'd1), .clk(clk), .reset(reset));
-  register32 PC_EX_MEM(.q(PC_3), .d(PC_2), .wrenable(1'd1), .clk(clk), .reset(reset));
-  register32 PC_MEM_WB(.q(PC_ready), .d(PC_3), .wrenable(1'd1), .clk(clk), .reset(reset));
+  reg en = 1;
+  wire [31:0] PC_1, PC_2, PC_3;
+  register32 PC_IF_RF(.q(PC_1), .d(PC), .wrenable(en), .clk(clk), .reset(reset));
+  register32 PC_RF_EX(.q(PC_2), .d(PC_1), .wrenable(en), .clk(clk), .reset(reset));
+  register32 PC_EX_MEM(.q(PC_3), .d(PC_2), .wrenable(en), .clk(clk), .reset(reset));
+  register32 PC_MEM_WB(.q(PC_ready), .d(PC_3), .wrenable(en), .clk(clk), .reset(reset));
 
-  var_register RS_IF_RF (#5)(.q(RS_ready), .d(RS), .wrenable(1'd1), .clk(clk), .reset(reset));
+  var_register #(5) RS_IF_RF(.q(RS_ready), .d(RS), .wrenable(en), .clk(clk), .reset(reset));
 
-  var_register RT_IF_RF (#5)(.q(RT_1), .d(RT), .wrenable(1'd1), .clk(clk), .reset(reset));
-  var_register RT_RF_EX (#5)(.q(RT_2), .d(RT_1), .wrenable(1'd1), .clk(clk), .reset(reset));
-  var_register RT_EX_MEM (#5)(.q(RT_3), .d(RT_2), .wrenable(1'd1), .clk(clk), .reset(reset));
-  var_register RT_MEM_WB (#5)(.q(RT_ready), .d(RT_3), .wrenable(1'd1), .clk(clk), .reset(reset));
+  wire [4:0] RT_1, RT_2, RT_3;
+  var_register #(5) RT_IF_RF(.q(RT_1), .d(RT), .wrenable(en), .clk(clk), .reset(reset));
+  var_register #(5) RT_RF_EX(.q(RT_2), .d(RT_1), .wrenable(en), .clk(clk), .reset(reset));
+  var_register #(5) RT_EX_MEM(.q(RT_3), .d(RT_2), .wrenable(en), .clk(clk), .reset(reset));
+  var_register #(5) RT_MEM_WB(.q(RT_ready), .d(RT_3), .wrenable(en), .clk(clk), .reset(reset));
 
-  var_register RD_IF_RF (#5)(.q(RD_1), .d(RD), .wrenable(1'd1), .clk(clk), .reset(reset));
-  var_register RD_RF_EX (#5)(.q(RD_2), .d(RD_1), .wrenable(1'd1), .clk(clk), .reset(reset));
-  var_register RD_EX_MEM (#5)(.q(RD_3), .d(RD_2), .wrenable(1'd1), .clk(clk), .reset(reset));
-  var_register RD_MEM_WB (#5)(.q(RD_ready), .d(RD_3), .wrenable(1'd1), .clk(clk), .reset(reset));
+  wire [4:0] RD_1, RD_2, RD_3;
+  var_register #(5) RD_IF_RF(.q(RD_1), .d(RD), .wrenable(en), .clk(clk), .reset(reset));
+  var_register #(5) RD_RF_EX(.q(RD_2), .d(RD_1), .wrenable(en), .clk(clk), .reset(reset));
+  var_register #(5) RD_EX_MEM(.q(RD_3), .d(RD_2), .wrenable(en), .clk(clk), .reset(reset));
+  var_register #(5) RD_MEM_WB(.q(RD_ready), .d(RD_3), .wrenable(en), .clk(clk), .reset(reset));
 
-  var_register imm_IF_RF (#16)(.q(imm_ready), .d(imm), .wrenable(1'd1), .clk(clk), .reset(reset));
+  var_register #(16) imm_IF_RF(.q(imm_ready), .d(imm), .wrenable(en), .clk(clk), .reset(reset));
 
-  var_register Wren_IF_RF (#16)(.q(Wren0), .d(Wren), .wrenable(1'd1), .clk(clk), .reset(reset));
-  var_register Wren_RF_EX (#16)(.q(Wren1), .d(Wren0), .wrenable(1'd1), .clk(clk), .reset(reset));
-  var_register Wren_EX_MEM (#16)(.q(Wren2), .d(Wren1), .wrenable(1'd1), .clk(clk), .reset(reset));
-  var_register Wren_MEM_WB (#16)(.q(Wren_ready), .d(Wren2), .wrenable(1'd1), .clk(clk), .reset(reset));
+  wire Wren0, Wren1, Wren2;
+  register Wren_IF_RF(.q(Wren0), .d(Wren), .wrenable(en), .clk(clk));
+  register Wren_RF_EX(.q(Wren1), .d(Wren0), .wrenable(en), .clk(clk));
+  register Wren_EX_MEM(.q(Wren2), .d(Wren1), .wrenable(en), .clk(clk));
+  register Wren_MEM_WB(.q(Wren_ready), .d(Wren2), .wrenable(en), .clk(clk));
 
-  var_register ALU_IF_RF (#16)(.q(ALU1), .d(ALU_ctrl), .wrenable(1'd1), .clk(clk), .reset(reset));
-  var_register ALU_RF_EX (#16)(.q(ALU_ctrl_ready), .d(ALU1), .wrenable(1'd1), .clk(clk), .reset(reset));
+  wire [2:0] ALU1;
+  var_register #(3) ALU_IF_RF(.q(ALU1), .d(ALU_ctrl), .wrenable(en), .clk(clk), .reset(reset));
+  var_register #(3) ALU_RF_EX(.q(ALU_ctrl_ready), .d(ALU1), .wrenable(en), .clk(clk), .reset(reset));
 
-  var_register MemtoReg_IF_RF (#16)(.q(MemtoReg0), .d(MemtoReg), .wrenable(1'd1), .clk(clk), .reset(reset));
-  var_register MemtoReg_RF_EX (#16)(.q(MemtoReg1), .d(MemtoReg0), .wrenable(1'd1), .clk(clk), .reset(reset));
-  var_register MemtoReg_EX_MEM (#16)(.q(MemtoReg2), .d(MemtoReg1), .wrenable(1'd1), .clk(clk), .reset(reset));
-  var_register MemtoReg_MEM_WB (#16)(.q(MemtoReg_ready), .d(MemtoReg2), .wrenable(1'd1), .clk(clk), .reset(reset));
+  wire MemtoReg0, MemtoReg1, MemtoReg2;
+  register MemtoReg_IF_RF(.q(MemtoReg0), .d(MemtoReg), .wrenable(en), .clk(clk));
+  register MemtoReg_RF_EX(.q(MemtoReg1), .d(MemtoReg0), .wrenable(en), .clk(clk));
+  register MemtoReg_EX_MEM(.q(MemtoReg2), .d(MemtoReg1), .wrenable(en), .clk(clk));
+  register MemtoReg_MEM_WB(.q(MemtoReg_ready), .d(MemtoReg2), .wrenable(en), .clk(clk));
 
-  var_register MemWren_IF_RF (#16)(.q(MemWren0), .d(MemWren), .wrenable(1'd1), .clk(clk), .reset(reset));
-  var_register MemWren_RF_EX (#16)(.q(MemWren1), .d(MemWren0), .wrenable(1'd1), .clk(clk), .reset(reset));
-  var_register MemWren_EX_MEM (#16)(.q(MemWren2), .d(MemWren1), .wrenable(1'd1), .clk(clk), .reset(reset));
-  var_register MemWren_MEM_WB (#16)(.q(MemWren_ready), .d(MemWren2), .wrenable(1'd1), .clk(clk), .reset(reset));
+  wire MemWren0, MemWren1, MemWren2;
+  register MemWren_IF_RF(.q(MemWren0), .d(MemWren), .wrenable(en), .clk(clk));
+  register MemWren_RF_EX(.q(MemWren1), .d(MemWren0), .wrenable(en), .clk(clk));
+  register MemWren_EX_MEM(.q(MemWren2), .d(MemWren1), .wrenable(en), .clk(clk));
+  register MemWren_MEM_WB(.q(MemWren_ready), .d(MemWren2), .wrenable(en), .clk(clk));
 
 
 endmodule
